e6d23d67f11b25661e260b75e338a75fc31182d3
“Parthav46” <parthav46@gmail.com>
2019-09-21 20:26:52 +0530

    cards and s'more jquery

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-// enable smooth scroll
+// run function
 $(function() {
+    // activate smooth scrolls
     $("html").css({"scroll-behavior" : "smooth"});
+
+    // home button click listener
+    $(".home").click(function () {
+        $("#0").focus();
+        if($("#navSupport").hasClass("show")) $(".navbar-toggler").trigger("click");
+    });
+
+    // close nav links drawer
+    $(".nav-link").click(function () {
+        if($("#navSupport").hasClass("show")) $(".navbar-toggler").trigger("click");
+    });
+
+    // testing function for keypress
+    $("body").keydown(function (ev) {
+        let k = ev.which;
+        console.log(k + " pressed");
+        let c = $("#counter").get(0);
+        if(k === 39) c.innerText = eval(c.innerText + " + 1");
+        else if (k === 37) c.innerText = eval(c.innerText + " - 1");
+    });
 });
+
