fbad9067da174f1a171de6a7f5531eb880146dee
Parthav46 <parthav46@gmail.com>
2023-11-19 22:57:52 +0530

    modify theme. create headers component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import '../css/Header.css';
 
-function Header() {
+export type HeaderObjectType = {
+    title: String,
+    isSelected?: Boolean
+}
+
+type HeaderProps = {
+    headers: { [key: string]: HeaderObjectType },
+    onHeaderSelect?: (key: string) => void,
+}
+
+function Header({ headers, onHeaderSelect }: HeaderProps) {
+
     return (
-        <>
-        Hello
-        </>
-    );
+        <header className='Header'>
+            {
+                Object.entries(headers).map(([key, value]) => (
+                    <span
+                        className={value.isSelected && 'selected'}
+                        onClick={() => onHeaderSelect && onHeaderSelect(key)} >
+                        {value.title}
+                    </span>
+                ))
+            }
+        </header>
+    )
 }
 
 export default Header;
--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ThemeManager from '../components/ThemeManager';
-import Header from '../components/Header';
+import Header, { HeaderObjectType } from '../components/Header';
 
 function App ()
 {
+    const [components, updateComponent] = useState<{[key: string]: HeaderObjectType}>({
+        'bio': { title: 'Bio' },
+        'skills': { title: 'Skills' },
+        'experience': { title: 'Experience' },
+        'projects': { title: 'Projects' }
+    });
+
+    useEffect(() => {
+        focusSection('bio')
+    }, []);
+
+    const focusSection = (id: string): void => {
+        let updatedComponent: {[key: string]: HeaderObjectType} = {};
+        Object.entries(components).forEach(([key, value]) => {
+            let isSelected = key === id;
+            updatedComponent[key] = { ...value, isSelected: isSelected };
+        });
+        updateComponent(updatedComponent);
+    }
+
     return (
         <ThemeManager>
-            <Header />
+            <Header headers={components} onHeaderSelect={focusSection} />
         </ThemeManager>
     )
 }
